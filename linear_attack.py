b88c79508b3c43fcf12697a917e4de41eab51063
tristanguerand <tristan@guerand.net>
2021-08-18 09:44:18 +0200

    add equation computing, not sure why but seems useful

--- linear_attack.py
+++ linear_attack.py
@@ -5,3 +5,26 @@ Created on Mon Aug 16 09:28:18 2021
 @author: trist
 """
 
+def str_xor(bit1, bit2):
+    
+    return bin(bool(int(bit1)) ^ bool(int(bit2)))[2:]
+
+def linear_equation(X, Y):
+    """Compute the linear XOR sum of all the bits of X and Y
+    
+    Args: 
+        X, hex str
+        Y, hex str"""
+        
+    bin_X = bin(int(X, 16))[2:]
+    bin_Y = bin(int(Y, 16))[2:]
+    
+    bin_XY = bin_X + bin_Y
+    
+    last = bin_XY[0]
+    
+    for i in range(1,len(bin_XY)):
+        
+        last = str_xor(last, bin_XY[i])
+        
+    return last
